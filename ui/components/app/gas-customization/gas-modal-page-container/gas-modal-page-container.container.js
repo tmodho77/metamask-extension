89535022eaac9a5551d46e52663fd4a55175fd8f
Alex <adonesky@gmail.com>
2021-05-27 10:28:50 -0500

    use fiat as primary for tokens too

--- ui/components/app/gas-customization/gas-modal-page-container/gas-modal-page-container.container.js
+++ ui/components/app/gas-customization/gas-modal-page-container/gas-modal-page-container.container.js
@@ -144,7 +144,6 @@ const mapStateToProps = (state, ownProps) => {
     customGasPrice,
     customGasLimit: calcCustomGasLimit(customModalGasLimitInHex),
     customGasTotal,
-    newTotalFiat,
     customPriceIsSafe:
       (isMainnet || process.env.IN_TEST) && isGasEstimate
         ? isCustomPriceSafe(state)
